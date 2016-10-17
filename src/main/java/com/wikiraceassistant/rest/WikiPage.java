c063f929494bad888fcd1c218df16c5700d0a4a3
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-10-16 21:36:19 -0500

    Added web framework and jsoup dependency

--- src/main/java/com/wikiraceassistant/rest/WikiPage.java
+++ src/main/java/com/wikiraceassistant/rest/WikiPage.java
@@ -1,8 +1,20 @@
 package com.wikiraceassistant.rest;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
 /**
  * Class to get a single page from Wikipedia
  * @author Sebastian Greenholtz
  */
+@Path("/pages")
 public class WikiPage {
+
+    @GET
+    @Path("{page}")
+    public Response getEventByID(@PathParam("page") String page) {
+        return Response.status(200).entity(page).build();
+    }
 }
