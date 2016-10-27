2feffd6ca624a548509f3fb7077225fe13fb59ac
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-10-26 19:33:08 -0500

    Updated ReadMe with tasks

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # WikiRaceAssistant
 Web service scrapes Wikipedia to find all the links on the page to help you with Wiki Race-ing
+
+- unit test to test the service
+- make sure it only searches once
+- format response as a JSON
+- front end
+- accept the keyword and make into link to search
+- error message
--- src/main/java/com/wikiraceassistant/crawler/CrawlerTest.java
+++ src/main/java/com/wikiraceassistant/crawler/CrawlerTest.java
@@ -7,6 +7,7 @@ package com.wikiraceassistant.crawler;
 public class CrawlerTest {
     public static void main(String[] args) {
         Crawler crawler = new Crawler();
-        crawler.search("http://isthmus.com/search/event/calendar-of-events/", "jazz");
+        crawler.search("https://en.wikipedia.org/wiki/Adolf_Hitler", "Nazi");
+        System.out.println(crawler.pagesToVisit);
     }
 }
