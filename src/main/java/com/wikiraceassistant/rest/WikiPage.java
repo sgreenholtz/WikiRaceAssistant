e2a1720bb20f2f1f703cda94fc37f62a65ad0d6b
Sebastian Greenholtz <smgreenholtz@gmail.com>
2016-10-27 18:13:55 -0500

    Added error handling within the rest controller

--- src/main/java/com/wikiraceassistant/rest/WikiPage.java
+++ src/main/java/com/wikiraceassistant/rest/WikiPage.java
@@ -1,5 +1,8 @@
 package com.wikiraceassistant.rest;
 
+import com.wikiraceassistant.crawler.Crawler;
+import com.wikiraceassistant.utility.JSONParser;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -16,7 +19,22 @@ public class WikiPage {
     @GET
     @Path("{page}")
     @Produces("text/json")
-    public Response getEventByID(@PathParam("page") String page) {
-        return Response.status(200).entity(page).build();
+    public Response getLinkList(@PathParam("page") String page) {
+        Crawler crawler = new Crawler();
+        crawler.search(page);
+        String json = JSONParser.parse(crawler.getLinks());
+        return constructResponse(json);
+    }
+
+    private Response constructResponse(String json) {
+        if (json != null) {
+            return Response.status(200).entity(json).build();
+        } else {
+            return constructResponse(404);
+        }
+    }
+
+    private Response constructResponse(Integer error) {
+        return Response.status(error).build();
     }
 }
