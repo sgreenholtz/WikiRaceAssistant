4cd9e6c04148e0943e6fe2871de814e42143dcac
Austin Chappell <amchappell9@gmail.com>
2016-11-06 11:13:17 -0600

    Front end list of links works

--- frontEnd/index.jsp
+++ frontEnd/index.jsp
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <title>Wiki Race Assistant</title>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script src="wra.js" charset="utf-8"></script>
-    </head>
-    <body>
-        <h1>Welecome to Wiki Race Assistant!</h1>
-        <label for="urlButton">Enter Url Here:</label>
-        <input type="text" id="urlInput" value="" />
-        <button id="submitButton">Submit</button>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <title>Wiki Race Assistant</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="wra.js" charset="utf-8"></script>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <h1>Welecome to Wiki Race Assistant!</h1>
+    <label for="urlButton">Enter Url Here:</label>
+    <input type="text" id="urlInput" value="" />
+    <button id="submitButton">Submit</button>
+    <div id="resultsContainer">
+        
+    </div>
+</body>
+
 </html>
--- frontEnd/wra.js
+++ frontEnd/wra.js
@@ -1,27 +1,65 @@
 (function($) {
+        const restURL = "http://localhost:8080/links/";
 
-  $(document).ready(function() {
-      var urlInput = $('#urlInput');
-      var submitButton = $('#submitButton');
-      console.log("in main function");
+        $(document).ready(function() {
+            var urlInput = $('#urlInput');
+            var submitButton = $('#submitButton');
+            console.log("in main function");
 
-      submitButton.click(function() {
-          // Ajax call to the REST service goes here
-          testAjaxCall();
+            submitButton.click(function() {
+                // Ajax call to the REST service goes here
+                console.log(urlInput.val());
+                getResultsByUrl(urlInput.val());
 
-      });
-  })
+            });
+        });
+
+        function getResultsByUrl(searchTerm) {
+            var resultsContainer = $('#resultsContainer');
+            var url = restURL + searchTerm;
+            console.log(url);
+            var list = $('<ul>');
 
 
+            $.ajax({
+                    url: url,
+                    dataType: 'json',
+                    success: function(result) {
+                        for (var i = 0; i < result.links.length; i++) {
+                            var aLink = $('<a>', {
+                                text: result.links[i].title,
+                                href: result.links[i].url
+                            });
+                            $('<li>').html(aLink).appendTo(list);
 
-  function testAjaxCall() {
-      console.log("in test call");
+                        }
+                    }
 
-      $.ajax({url: "http://localhost:8080/links/hitler", success: function(result){
-            console.log(result);
-        },
-        crossDomain: true,
-    });
+                });
 
-  }
-})(jQuery)
+                list.appendTo(resultsContainer);
+
+
+            }
+
+            function testAjaxCall() {
+                console.log("in test call");
+
+                $.ajax({
+                    url: "http://localhost:8080/links/Aeba_Koson",
+                    dataType: 'json',
+                    success: function(result) {
+                            console.log(result.links);
+                            console.log(result.links[0].title);
+                            console.log(result.links[0].url);
+                            console.log("Number of links: " + result.links.length);
+                            for (var i = 0; i < result.links.length; i++) {
+                                console.log(result.links[i].title);
+                                console.log(result.links[i].url);
+                            }
+                        }
+                        // crossDomain: true,
+                });
+
+            }
+        })(jQuery);
