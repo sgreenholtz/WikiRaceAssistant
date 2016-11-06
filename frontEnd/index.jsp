73bc7335027895ae5d3aacb037e5e8e76d371366
Austin Chappell <amchappell9@gmail.com>
2016-11-06 15:17:17 -0600

    Completed basic front end functionality and added styles

--- frontEnd/index.jsp
+++ frontEnd/index.jsp
@@ -4,26 +4,53 @@
 <head>
     <meta charset="utf-8">
     <title>Wiki Race Assistant</title>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
     <script src="wra.js" charset="utf-8"></script>
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
 
     <!-- Optional theme -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
     <!-- Latest compiled and minified JavaScript -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
-    <h1>Welecome to Wiki Race Assistant!</h1>
-    <label for="urlButton">Enter Url Here:</label>
-    <input type="text" id="urlInput" value="" />
-    <button id="submitButton">Submit</button>
-    <div id="resultsContainer">
-        
+    <div class="jumbotron jumbotron-fluid">
+        <div class="container">
+            <h1 class="display-3">Welcome to the Wiki Race Assistant!</h1>
+            <p class="lead">To get started, enter the last part of your Wikipedia url below.</p>
+        </div>
     </div>
+
+    <div class="container">
+
+        <form>
+            <div class="form-group row">
+                <label for="urlInput" class="col-xs-3 col-form-label text-right">Enter URL term:</label>
+                <div class="col-xs-5">
+                    <input class="form-control" type="text" id="urlInput" />
+                </div>
+                <div class="col-xs-2">
+                    <button type="button" class="btn btn-primary" id="submitButton">Submit</button>
+                </div>
+            </div>
+        </form>
+
+    </div>
+
+    <div class="container">
+        <div id="resultsContainer">
+
+        </div>
+    </div>
+
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" integrity="sha384-BLiI7JTZm+JWlgKa0M0kGRpJbF2J8q+qreVrKBC47e3K6BW78kGLrCkeRX6I9RoK" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- frontEnd/wra.js
+++ frontEnd/wra.js
@@ -1,65 +1,131 @@
+// TODO add click counter
+// TODO random article
+// TODO add styles
+
+
 (function($) {
-        const restURL = "http://localhost:8080/links/";
+    const restURL = "http://localhost:8080/links/";
 
-        $(document).ready(function() {
-            var urlInput = $('#urlInput');
-            var submitButton = $('#submitButton');
-            console.log("in main function");
 
-            submitButton.click(function() {
-                // Ajax call to the REST service goes here
-                console.log(urlInput.val());
+
+    $(document).ready(function() {
+        var resultsContainer = $('#resultsContainer');
+        var urlInput = $('#urlInput');
+        var submitButton = $('#submitButton');
+        console.log("in main function");
+
+        // Prevent enter button from submitting form incorrectly
+        $(window).keydown(function(event) {
+            if (event.keyCode == 13) {
+                event.preventDefault();
+                $('#resultsContainer').empty();
                 getResultsByUrl(urlInput.val());
-
-            });
+            }
         });
 
-        function getResultsByUrl(searchTerm) {
-            var resultsContainer = $('#resultsContainer');
-            var url = restURL + searchTerm;
-            console.log(url);
-            var list = $('<ul>');
+        submitButton.click(function(e) {
+            // Ajax call to the REST service goes here
+            console.log(urlInput.val());
+            $('#resultsContainer').empty();
+            getResultsByUrl(urlInput.val());
+
+        });
 
 
-            $.ajax({
-                    url: url,
-                    dataType: 'json',
-                    success: function(result) {
-                        for (var i = 0; i < result.links.length; i++) {
-                            var aLink = $('<a>', {
-                                text: result.links[i].title,
-                                href: result.links[i].url
-                            });
-                            $('<li>').html(aLink).appendTo(list);
+    });
 
-                        }
+    // Makes Ajax call to REST service and displays the results on the
+    // web page.
+    function getResultsByUrl(searchTerm) {
+        var url = restURL + searchTerm;
+        console.log(url);
+        var list = $('<ul>');
+        list.addClass("list-group");
+
+
+        $.ajax({
+            url: url,
+            dataType: 'json',
+            success: function(result) {
+                for (var i = 0; i < result.links.length; i++) {
+                    var aLink = $('<a>', {
+                        text: result.links[i].title,
+                        id: i,
+                        class: 'link list-group-item col-xs-6',
+                        href: '#'
+                    });
+                    aLink.data("url", result.links[i].url);
+                    aLink.click(function() {
+                        testFun(this);
+                        getNewResults(this);
+                    });
+                    $('<li>').html(aLink).appendTo(list);
+
+                }
+            }
+
+        });
+
+        list.appendTo(resultsContainer);
+
+    }
+
+    // Function that is called when a link on the screen is pressed
+    function getNewResults(clickedObject) {
+        // Get url from clickedObject
+        var url = $('#' + clickedObject.id).data("url");
+
+        // get searchTerm from url by calling getSearchTermFromURL
+        var searchTerm = getSearchTermFromURL(url);
+
+        // Clear previous results
+        $('#resultsContainer').empty();
+
+        // put search term in the input box
+        $('#urlInput').val(searchTerm);
+
+        // get new links from new searchTerm from getResultsByUrl
+        getResultsByUrl(searchTerm);
+    }
+
+    // Gets last part of the the Wikipedia URL.
+    function getSearchTermFromURL(url) {
+        return url.split('/').pop();
+    }
+
+
+
+
+    // Test functions
+    function testFun(object) {
+        // Gets url from link tag that was clicked.
+        // TODO use URL to make new call and display results
+        var objectId = object.id;
+        $('#' + objectId);
+        console.log($('#' + objectId).data("url"));
+    }
+
+    function testAjaxCall() {
+        console.log("in test call");
+
+        $.ajax({
+            url: "http://localhost:8080/links/Aeba_Koson",
+            dataType: 'json',
+            success: function(result) {
+                    console.log(result.links);
+                    console.log(result.links[0].title);
+                    console.log(result.links[0].url);
+                    console.log("Number of links: " + result.links.length);
+                    for (var i = 0; i < result.links.length; i++) {
+                        console.log(result.links[i].title);
+                        console.log(result.links[i].url);
                     }
+                }
+                // crossDomain: true,
+        });
 
-                });
-
-                list.appendTo(resultsContainer);
+    }
 
 
-            }
 
-            function testAjaxCall() {
-                console.log("in test call");
-
-                $.ajax({
-                    url: "http://localhost:8080/links/Aeba_Koson",
-                    dataType: 'json',
-                    success: function(result) {
-                            console.log(result.links);
-                            console.log(result.links[0].title);
-                            console.log(result.links[0].url);
-                            console.log("Number of links: " + result.links.length);
-                            for (var i = 0; i < result.links.length; i++) {
-                                console.log(result.links[i].title);
-                                console.log(result.links[i].url);
-                            }
-                        }
-                        // crossDomain: true,
-                });
-
-            }
-        })(jQuery);
+})(jQuery);
