05064a1666c36ef050cf223de165023ebcb8332b
Austin Chappell <amchappell9@gmail.com>
2016-11-13 15:28:53 -0600

    Starting Crawler Unit Test

---
+++ wikirace/src/test/java/com/wikiraceassistant/crawler/CrawlerUnitTest.java
@@ -0,0 +1,27 @@
+package com.wikiraceassistant.crawler;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 2-13-13 on 11/13/2016.
+ */
+public class CrawlerUnitTest {
+    @Before
+    public void setUp() throws Exception {
+        Crawler crawler = new Crawler();
+    }
+
+    @Test
+    public void searchTest() throws Exception {
+
+    }
+
+    @Test
+    public void getLinksTest() throws Exception {
+
+    }
+
+}
